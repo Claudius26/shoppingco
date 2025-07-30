222523091bc49a1920743a1169a4cb2dd32396ac
Claudius26 <osomzed@gmail.com>
2025-07-29 23:18:27 -0600

    tailwindcss added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,78 @@
-import React from 'react'
+// import React from 'react'
+// import searchIcon from '../../images/search.svg';
+// import cartIcon from '../../images/cart.svg';
+// import accountIcon from '../../images/account.svg';
+
+
+
+// const Navbar = () => {
+//   return (
+//     <div>
+//       <header>
+//         <h2>SHOP.CO</h2>
+//         <div>
+//           <p>Shop <span className="arrow">&#9662;</span></p>
+//           <p>On Sale</p>
+//           <p>New Arrivals</p>
+//           <p>Brands </p>
+//         </div>
+
+//         <div>
+//           <img src={searchIcon} alt="Search" />
+//           <input type="text"  placeholder='Search for products' />
+//         </div>
+
+//         <div>
+          
+//           <img src={cartIcon} alt="Cart" />
+//           <img src={accountIcon} alt="Account" />
+
+//         </div>
+//       </header>
+//     </div>
+//   )
+// }
+
+// export default Navbar
+
+import React from 'react';
 import searchIcon from '../../images/search.svg';
 import cartIcon from '../../images/cart.svg';
 import accountIcon from '../../images/account.svg';
 
-
-
 const Navbar = () => {
   return (
-    <div>
-      <header>
-        <h2>SHOP.CO</h2>
-        <div>
-          <p>Shop <span className="arrow">&#9662;</span></p>
-          <p>On Sale</p>
-          <p>New Arrivals</p>
-          <p>Brands </p>
-        </div>
+    <header className="flex items-center justify-between px-6 py-4 shadow-md bg-white">
+      {/* Logo */}
+      <h2 className="text-2xl font-bold text-gray-800">SHOP.CO</h2>
 
-        <div>
-          <img src={searchIcon} alt="Search" />
-          <input type="text"  placeholder='Search for products' />
-        </div>
+      {/* Navigation Links */}
+      <nav className="hidden md:flex space-x-6 text-gray-600 text-sm font-medium">
+        <p className="hover:text-black cursor-pointer">
+          Shop <span className="ml-1">&#9662;</span>
+        </p>
+        <p className="hover:text-black cursor-pointer">On Sale</p>
+        <p className="hover:text-black cursor-pointer">New Arrivals</p>
+        <p className="hover:text-black cursor-pointer">Brands</p>
+      </nav>
 
-        <div>
-          
-          <img src={cartIcon} alt="Cart" />
-          <img src={accountIcon} alt="Account" />
+      {/* Search Bar */}
+      <div className="hidden md:flex items-center border border-gray-300 rounded-md px-3 py-1 bg-gray-100">
+        <img src={searchIcon} alt="Search" className="w-4 h-4 mr-2" />
+        <input
+          type="text"
+          placeholder="Search for products"
+          className="bg-transparent focus:outline-none text-sm"
+        />
+      </div>
 
-        </div>
-      </header>
-    </div>
-  )
-}
+      {/* Icons */}
+      <div className="flex items-center space-x-4">
+        <img src={cartIcon} alt="Cart" classNaOPJHIme="w-5 h-5 cursor-pointer" />
+        <img src={accountIcon} alt="Account" className="w-5 h-5 cursor-pointer" />
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/signupheader.jsx/Header.jsx
+++ src/components/signupheader.jsx/Header.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import cancelButton from '../../images/cancel.svg'
-import { Link } from 'react-router';
+import React from 'react';
+import cancelButton from '../../images/cancel.svg';
+import { Link } from 'react-router'; 
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <p>Signup and get 20% off your first order. <Link to=''>Sign up now</Link></p>
-        <p>
-          <img src={cancelButton} alt="cancel" />
+    <div className="bg-black text-white">
+      <header className="flex items-center justify-between px-4 py-2">
+        <p className="mx-auto text-center text-sm">
+          Signup and get 20% off your first order.{' '}
+          <Link to="" className="underline text-white hover:text-gray-300">
+            Sign up now
+          </Link>
         </p>
+
+        <button className="absolute right-4">
+          <img src={cancelButton} alt="cancel" className="h-4 w-4" />
+        </button>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
