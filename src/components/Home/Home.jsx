7cfbd7ebd630031673f0db120fe990a8fdd9028f
Claudius26 <osomzed@gmail.com>
2025-08-25 05:55:20 +0100

    update the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import trendingicon from '../../images/trendingoutfit.jpg';
 import smallVector from '../../images/smallvector.svg';
 import vector from '../../images/Vector.svg';
